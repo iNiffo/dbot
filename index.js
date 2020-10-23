3b147146b0d246248cd7c6276ed981fd65354022
iNiffo <bluerodri.ab@gmail.com>
2020-10-23 15:46:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ client.on("ready", async () => {
 
 
     console.log(`${client.user.username} is online`);
-    client.user.setActivity("Jonko smoken", {type: "LISTENING"});
+    client.user.setActivity("Jonko smoken", {type: "COMPETING"});
 
 
 });
@@ -27,6 +27,10 @@ var command = messageArray[0];
 
 if(command === `${prefix}social`){
     return message.channel.send(" https://www.youtube.com/channel/UCsE_8xw20vgI0zO4EPy8E8w " + " https://www.twitch.tv/jxshytv " + " https://www.snapchat.com/add/joshuapaton " + " https://instagram.com/josh__paton?igshid=1gdo044stqyf2 ");         
+}
+
+if(command ===`${prefix}rogue`){
+    return message.channel.send("Use code Fakka for 10% off your next rogue energy purchase you fucking pepega https://www.rogueenergy.com/?rfsn=4654754.8d8456&utm_source=refersion&utm_medium=affiliate&utm_campaign=4654754.8d8456");
 }
 
 if(command === `${prefix}help`){
