fe5d744a2d4fae511c2c889230bda33954ca01df
SirMrStep <lemikahriman@gmail.com>
2020-10-24 03:00:16 +0200

    Purge command geadd
    dat is letterlijk alles.

--- index.js
+++ index.js
@@ -28,6 +28,26 @@ var messageArray = message.content.split(" ");
 
 var command = messageArray[0];
 
+if(command == `${prefix}purge`){
+
+const purgeArgs = message.content.split(' ').slice(1);
+const amount = purgeArgs.join(' '); // Vriend als je kan doe amount + 1 zodat het die command zelf
+                                    // niet meetelt! Elke keer als ik dat probeer gooit ie gare error
+                                    // die niet klopt.
+
+if (!amount) return message.reply('You haven\'t given an amount of messages which should be deleted!');
+if (isNaN(amount)) return message.reply('The amount parameter isn\'t a number!');
+
+if (amount > 100) return message.reply('You can\'t delete more than 100 messages at once!');
+if (amount < 1) return message.reply('You have to delete at least 1 message!');
+
+await message.channel.messages.fetch({ limit: amount }).then(messages => {
+message.channel.bulkDelete(messages)
+
+});
+
+}
+
 if(command === `${prefix}twitch`){
 
 var botEmbedTwitch = new discord.MessageEmbed()
